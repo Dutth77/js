b12828f6d4d470953bd610426618919e1bc6c4c0
Dutth77 <145188666+Dutth77@users.noreply.github.com>
2023-12-01 10:02:05 +0000

    Data Types

---
+++ 01_basics/dataTypes.js
@@ -0,0 +1,33 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are using node js,not browser
+
+console.log(3+3)
+
+console.log("Dutth")
+
+// https://tc39.es/ecma262/2022/
+// MDN
+// above is documantation for JS
+
+
+let name ="Dutth"
+let age = 18
+let isLoggedIn = false
+
+// Data Types
+
+// number => 2 to power 53
+// bigint 
+// string => "" or ''
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// object
+
+console.log(typeof null); //object
+console.log(typeof undefined); // undefined
