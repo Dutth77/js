ddcafdef2fb110e0cf8a53229fcc71515677b501
Dutth77 <145188666+Dutth77@users.noreply.github.com>
2023-12-04 17:49:55 +0000

    Update

--- 01_basics/Variable.js
+++ 01_basics/Variable.js
@@ -6,7 +6,6 @@ accountCity = "Jaipur"
 let accountState;
 // accountId = 2 // not allowed
 
-
 accountEmail = "hc@hc.com"
 accountPassword = "21212121"
 accountCity = "Bengaluru"
--- 01_basics/conversionOperation.js
+++ 01_basics/conversionOperation.js
@@ -1,9 +1,35 @@
 let score = "33abc"
 
 console.log(typeof score);
-console.log(typeof (score));
+console.log(typeof (score)); //() method
 
 
 let valueInNumber = Number(score) 
 console.log(typeof valueInNumber);
 console.log(valueInNumber);
+
+// NaN not a number
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "Dutth" => true
+
+//.........................................
+
+
+let someNumber = 55
+
+let numberString = String(someNumber)
+console.log(numberString);
+console.log(typeof numberString);
+
+
