9d9f17dbae5c0507a40848b153587054fb158a54
Dutth77 <145188666+Dutth77@users.noreply.github.com>
2024-01-26 18:40:54 +0000

    Arrow

---
+++ 03_Functions/03_arrow.js
@@ -0,0 +1,23 @@
+// const user ={
+//     username:"Dutth",
+//     price:199,
+//     welcomemessage:function() {
+//         console.log(`${this.username} welcome to website`);
+//         console.log(this);
+//     }
+
+// }
+// user.welcomemessage()
+
+// function chai() {
+// console.log(this);    
+// }
+// chai()
+
+// Arrow function
+
+// let dutth = () =>{
+//     console.log(`hello world`);
+// }
+// dutth();
+
